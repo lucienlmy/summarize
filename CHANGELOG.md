dad21955b3ecd8a6ec0dc95736108dcb8712b1cc
Peter Steinberger <steipete@gmail.com>
2026-06-11 17:29:09 -0400

    chore: release 0.17.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## 0.17.2 - Unreleased
+## 0.17.2 - 2026-06-11
 
 ### Features
 
--- src/version.ts
+++ src/version.ts
@@ -4,7 +4,7 @@ import { fileURLToPath } from "node:url";
 
 declare const __dirname: string | undefined;
 
-export const FALLBACK_VERSION = "0.17.1";
+export const FALLBACK_VERSION = "0.17.2";
 
 export function resolvePackageVersion(importMetaUrl?: string): string {
   const injected =
